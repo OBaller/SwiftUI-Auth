75e666062b055e9e86c66db69f34e2190a45786f
brightsummit.co <naseemoyebola@gmail.com>
2021-09-14 00:23:43 +0100

    viewModel

--- SwiftUIAuth/SwiftUIAuth/SwiftUIAuthApp.swift
+++ SwiftUIAuth/SwiftUIAuth/SwiftUIAuthApp.swift
@@ -4,14 +4,25 @@
 //
 //  Created by IBE on 13/09/2021.
 //
-
+import Firebase
 import SwiftUI
 
 @main
 struct SwiftUIAuthApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
+            let viewModel = AppViewModel()
             ContentView()
+                .environmentObject(viewModel)
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
---
+++ SwiftUIAuth/SwiftUIAuth/ViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  ViewModel.swift
+//  SwiftUIAuth
+//
+//  Created by IBE on 14/09/2021.
+//
+import SwiftUI
+import Foundation
+import FirebaseAuth
+
+
+class AppViewModel: ObservableObject {
+    let auth = Auth.auth()
+    var isSignedIn: Bool {
+        return auth.currentUser != nil
+    }
+    
+    func signIn(email: String, password: String) {
+        auth.signIn(withEmail: email, password: password) { (result, error) in
+            guard result != nil, error == nil else {
+                return
+            }
+            // success
+        }
+    }
+    
+    func signUp(email: String, password: String) {
+        auth.createUser(withEmail: email, password: password) { (result, error) in
+            guard result != nil, error == nil else {
+                return
+            }
+            // success
+        }
+    }
+}
